9361e9623df44aff95dcca4dc6cc9a29f9cc9411
miguelss3 <miguelss3@yahoo.com.br>
2026-03-19 09:54:24 -0400

    atualização do aluno

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,13 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
+
+// Importação das suas páginas
 import Index from "./pages/Index.tsx";
 import Aluno from "./pages/Aluno.tsx";
 import Aula from "./pages/Aula.tsx";
 import Painel from "./pages/Painel.tsx";
+import RedefinirSenha from "./pages/RedefinirSenha.tsx"; // <-- 1. IMPORTAMOS A NOVA PÁGINA AQUI
 import NotFound from "./pages/NotFound.tsx";
 
 const queryClient = new QueryClient();
@@ -22,6 +25,10 @@ const App = () => (
           <Route path="/aluno" element={<Aluno />} />
           <Route path="/aula" element={<Aula />} />
           <Route path="/painel" element={<Painel />} />
+          
+          {/* <-- 2. ADICIONAMOS A ROTA OFICIAL DA NOVA PÁGINA AQUI --> */}
+          <Route path="/redefinir-senha" element={<RedefinirSenha />} /> 
+          
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
